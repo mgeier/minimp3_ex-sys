5cc6f15c078f7319a83ad3cbe07c10d2da266643
Matthias Geier <Matthias.Geier@gmail.com>
2020-08-19 11:17:46 +0200

    bindgen script: remove unnecessary -I flag

--- bindgen/run-bindgen.sh
+++ bindgen/run-bindgen.sh
@@ -18,7 +18,7 @@ bindgen wrapper.h -o ../src/bindings.rs \
 	--whitelist-var "MP3D_.*" \
 	--no-copy ".*" \
 	--blacklist-type mp3d_sample_t \
-	-- -I../minimp3
+	--
 
 sed -i 's/pub const MP3D_SEEK_TO_\(\w*\): u32/pub const MP3D_SEEK_TO_\1: libc::c_int/' ../src/bindings.rs
 sed -i 's/pub const MP3D_DO_NOT_SCAN: u32/pub const MP3D_DO_NOT_SCAN: libc::c_int/' ../src/bindings.rs
